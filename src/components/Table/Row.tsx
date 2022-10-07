f0dcd983ba9620fc1ccac63ea233cdb7fd0cd4d2
BrunoPanizzi <brunopanizziq@gmail.com>
2022-10-06 22:37:47 -0300

    sort of solved weird input behaivor on firefox

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -7,31 +7,51 @@ interface rowProps {
 }
 
 export const StockRow: Component<rowProps> = ({ stock }) => {
+  let inpRef: HTMLInputElement = null
+
+  let weirdRemember = weirdClojure((prev: string, value: string) => {
+    if (!isNaN(Number(value)) && value.trim() === value) {
+      inpRef.value = value
+      changeAmount(stock.ticker, Number(value) || 0)
+      return value
+    }
+    inpRef.value = prev
+    return prev
+  })
+
   return (
     <tr>
       <td>{stock.ticker}</td>
       <td>
         <input
           class="bg-transparent border w-full focus:outline-0 focus:border-slate-800"
-          type="number"
           min={0}
-          value={stock.amount || 0}
+          value={stock.amount}
           onInput={(e) => {
-            changeAmount(stock.ticker, e.currentTarget.valueAsNumber || 0)
+            weirdRemember(e.currentTarget.value)
           }}
+          ref={inpRef}
         />
       </td>
       <td>
-        <Show when={!stock.fetched} fallback={'loading...'}>
+        <Show when={stock.fetched} fallback={'loading...'}>
           {stock.price}
         </Show>
       </td>
       <td>
-        <Show when={!stock.fetched} fallback={'loading...'}>
+        <Show when={stock.fetched} fallback={'loading...'}>
           {stock.price}
         </Show>
       </td>
       <td>{stock.weight}</td>
     </tr>
   )
+}
+
+function weirdClojure<T>(callback: (prev: T, curr: T) => T) {
+  let prev: T = null
+
+  return (value: T) => {
+    prev = callback(prev, value)
+  }
 }
